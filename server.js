f4909230f2af210f0ac4068d4889aeb9c4505182
aakash1102 <aakash.sing01@yahoo.com>
2022-03-23 15:12:39 +0530

    added new angular profile management system

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-assignment-aakash'));
+app.use(express.static(__dirname+'/dist/angular-assignment-aakash'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-assignment-aakash'}),
+    // res.sendFile('index.html', {root: 'dist/angular-assignment-aakash'}),
+    res.sendFile(path.join(__dirname+'/dict/angular-assignment-aakash/index.html'))
 );
 
 // Start the app by listening on the default Heroku port
